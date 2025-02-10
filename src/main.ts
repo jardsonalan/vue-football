09cc3d49973849de431490df47fcb5f9ae6c1b8c
Jardson Alan <jardsonalan132@gmail.com>
2025-02-10 13:24:19 -0300

    feat: subindo projeto

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+
+import '@mdi/font/css/materialdesignicons.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import App from './App.vue'
+import router from './router'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+  },
+})
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
