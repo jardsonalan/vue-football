03acd3dfcfb86dd60b7dbb872d7c8a6f5dad3776
Jardson Alan <jardsonalan132@gmail.com>
2025-02-10 16:34:02 -0300

    feat: consumindo API para feed de vídeos

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+// import { getFeed, partidas, videos } from '@/api/scorebat.ts';
+// import { onMounted } from 'vue';
+import Feed from './components/Feed.vue';
+
+// onMounted(getFeed);
 </script>
 
 <template>
-  <h1></h1>
+  <Feed />
 </template>
 
 <style scoped>
---
+++ src/api/scorebat.ts
@@ -0,0 +1,48 @@
+import { ref } from "vue";
+import axios from "axios";
+
+interface Video {
+  readonly id: number;
+  title: string;
+  embed: string;
+}
+
+interface Partida {
+  title: string;
+  competition: string;
+  matchviewUrl: string;
+  competitionUrl: string;
+  thumbnail: string;
+  date: string;
+  videos: Video[];
+}
+
+interface ApiResponse {
+  response: Partida[];
+}
+
+// Token da Aplicação
+const token = 'MTk4MDEzXzE3MzkyMDY1MzVfYTc2NzI2NmYxM2I4ZDI5OGM5ZTQyOGI0MjMzZDFiOTRiODU5YjMzOA==';
+
+// Listas de Objetos com os atributos da API
+export const partidas = ref<Partida[]>([]);
+export const videos = ref<Video[]>([]);
+
+// Função para buscar as últimas publicações do Feed
+export async function getFeed() {
+  await axios.get<ApiResponse>(`https://www.scorebat.com/video-api/v3/feed/?token=${token}`)
+  .then(response => {
+    const data = response.data;
+
+    data.response.forEach((partida: Partida) => {
+      partidas.value.push(partida)
+
+      partida.videos.forEach((video: Video) => {
+        videos.value.push(video)
+      })
+    })
+  })
+  .catch(error => {
+    console.error('Erro ao consumir os dados: ', error);
+  })
+}
