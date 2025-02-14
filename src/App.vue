81fe4924c1f3e91d28eddcdfd576571d58a45940
Jardson Alan <jardsonalan132@gmail.com>
2025-02-13 22:02:00 -0300

    feat: atualização do favicon; adicão do componente Header; modificação do App.vue para incluir o Header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import Header from './components/Header.vue';
 import Feed from './views/Feed.vue';
 import Footer from './components/Footer.vue';
 </script>
 
 <template>
+  <header class="bg-grey-darken-4 pa-2 text-center">
+    <Header
+      title="Vue FootBall"
+    />
+  </header>
+
   <main>
     <Feed />
   </main>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+defineProps({
+  title: String
+})
+</script>
+
+<template>
+  <VContainer>
+    <VRow>
+      <VCol>
+        <h1>{{ title }}</h1>
+      </VCol>
+    </VRow>
+  </VContainer>
+</template>
+
+<style scoped>
+h1 {
+  color: #00E676;
+}
+</style>
